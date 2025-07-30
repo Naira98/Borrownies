af0afde5ab4713ab3d288df7a8fdb952797f414d
MohamedAbdrabou12 <mohamedabdrabou840@gmail.com>
2025-07-30 12:40:39 +0300

    refactor navbar component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,15 @@ import Home from './pages/Home';
 import Register from './pages/Register';
 import LoginPage from './pages/LoginPage';
 import EmailVerification from './pages/EmailVerification';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
      <BrowserRouter>
          <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<><Navbar /><Home /></>} />
+          <Route path="/email_verification" element={<><Navbar /><EmailVerification /></>} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/email_verification" element={<EmailVerification />} />
@@ -16,45 +19,4 @@ const App = () => {
      </BrowserRouter>
   );
 };
-
 export default App;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Routes, Route, BrowserRouter} from 'react-router-dom';
-// import Home from './pages/Home';
-// import Register from './pages/Register';
-// import Login from './pages/Login';
-// import EmailVerification from './pages/EmailVerification';
-
-
-// const App = () => {
-//   return (
-//      <BrowserRouter>
-//          <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/register" element={<Register />} />
-//           <Route path="/login" element={<Login />} />
-//           <Route path="/email_verification" element={<EmailVerification />} />
-//         </Routes>
-//      </BrowserRouter>
-//   );
-// };
-// export default App;
